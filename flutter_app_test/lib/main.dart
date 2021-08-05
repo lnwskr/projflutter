fb541b7e894c2fcee8ca9d206e55e2979c315bbe
lnwskr <86812975+lnwskr@users.noreply.github.com>
2021-08-05 08:14:39 +0700

    refactor001

--- flutter_app_test/lib/main.dart
+++ flutter_app_test/lib/main.dart
@@ -1,97 +1,173 @@
 import 'dart:io';
-import 'package:image_picker/image_picker.dart';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    title: "Camera App",
-    home: LandingScreen(),
-  ));
-}
+void main() => runApp(MyApp());
 
-class LandingScreen extends StatefulWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
-  _LandingScreenState createState() => _LandingScreenState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.red,
+      ),
+      home: MyHomePage(/*title: 'FirztAir'*/),
+    );
+  }
 }
 
-class _LandingScreenState extends State<LandingScreen> {
-  File  imageFile = File('');
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
-  _openGallery(BuildContext context) async {
-    var picture = await ImagePicker().getImage(source: ImageSource.gallery);
-    this.setState(() {
-      imageFile = picture as File;
-    });
-    Navigator.of(context).pop();
-  }
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-  _openCamera(BuildContext context) async {
-    var picture = await ImagePicker().getImage(source: ImageSource.camera);
-    this.setState(() {
-      imageFile = picture as File;
-    });
-    Navigator.of(context).pop();
-  }
-
-  Future<void> _showChoiceDialog(BuildContext context) {
-    return showDialog(
-        context: context,
-        builder: (BuildContext) {
-          return AlertDialog(
-            title: Text("Make a choice"),
-            content: SingleChildScrollView(
-              child: ListBody(
-                children: <Widget>[
-                  GestureDetector(
-                    child: Text("Gallery"),
-                    onTap: () {
-                      _openGallery(context);
-                    },
-                  ),
-                  Padding(padding: EdgeInsets.all(8.0)),
-                  GestureDetector(
-                    child: Text("Camera"),
-                    onTap: () {
-                      _openCamera(context);
-                    },
-                  )
-                ],
-              ),
-            ),
-          );
-        });
-  }
-
-  Widget _decideImageView() {
-    if (imageFile == Null) {
-      return Text("No Image Selected");
-    } else {
-      Image.file(imageFile, width: 400, height: 400);
-    }
-    return Text("");
-  }
+class _MyHomePageState extends State<MyHomePage> {
+  final ImagePicker _picker = ImagePicker();
+  XFile? image;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Main Screen"),
-      ),
-      body: Container(
+      body: SafeArea(
           child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            _decideImageView(),
-            RaisedButton(
-              onPressed: () {
-                _showChoiceDialog(context);
-              },
-              child: Text("Select Image"),
-            )
-          ],
-        ),
-      )),
+            child: Column(
+              children: [
+                TextButton(
+        child: const Text('Select Image'),
+        onPressed: () {
+          filePicker(); //function
+        },
+      ),
+      const SizedBox(height: 20,),
+      image==null ? Text("No Image Found") : // if image == null
+      Image.file(File(image!.path),width: 250,fit: BoxFit.cover,) 
+      ],
+    ))),
     );
   }
+
+  void filePicker() async {
+    final XFile? selectImage =
+        await _picker.pickImage(source: ImageSource.gallery);
+    // print(selectImage!.path);
+    setState(() {
+      image = selectImage;
+    });
+  }
 }
+
+
+
+// import 'dart:io';
+// import 'package:image_picker/image_picker.dart';
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(new MaterialApp(
+//     title: "Camera App",
+//     home: LandingScreen(),
+//   ));
+// }
+//
+// class LandingScreen extends StatefulWidget {
+//   @override
+//   _LandingScreenState createState() => _LandingScreenState();
+// }
+//
+// class _LandingScreenState extends State<LandingScreen> {
+//   File  imageFile = File('');
+//
+//   _openGallery(BuildContext context) async {
+//     var picture = await ImagePicker().getImage(source: ImageSource.gallery);
+//     this.setState(() {
+//       imageFile = picture as File;
+//     });
+//     Navigator.of(context).pop();
+//   }
+//
+//   _openCamera(BuildContext context) async {
+//     var picture = await ImagePicker().getImage(source: ImageSource.camera);
+//     this.setState(() {
+//       imageFile = picture as File;
+//     });
+//     Navigator.of(context).pop();
+//   }
+//
+//   Future<void> _showChoiceDialog(BuildContext context) {
+//     return showDialog(
+//         context: context,
+//         builder: (BuildContext) {
+//           return AlertDialog(
+//             title: Text("Make a choice"),
+//             content: SingleChildScrollView(
+//               child: ListBody(
+//                 children: <Widget>[
+//                   GestureDetector(
+//                     child: Text("Gallery"),
+//                     onTap: () {
+//                       _openGallery(context);
+//                     },
+//                   ),
+//                   Padding(padding: EdgeInsets.all(8.0)),
+//                   GestureDetector(
+//                     child: Text("Camera"),
+//                     onTap: () {
+//                       _openCamera(context);
+//                     },
+//                   )
+//                 ],
+//               ),
+//             ),
+//           );
+//         });
+//   }
+//
+//   Widget _decideImageView() {
+//     if (imageFile == Null) {
+//       return Text("No Image Selected");
+//     } else {
+//       Image.file(imageFile, width: 400, height: 400);
+//     }
+//     return Text("");
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Main Screen"),
+//       ),
+//       body: Container(
+//           child: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.spaceAround,
+//           children: <Widget>[
+//             _decideImageView(),
+//             RaisedButton(
+//               onPressed: () {
+//                 _showChoiceDialog(context);
+//               },
+//               child: Text("Select Image"),
+//             )
+//           ],
+//         ),
+//       )),
+//     );
+//   }
+// }
